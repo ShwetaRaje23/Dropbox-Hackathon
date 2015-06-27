57759ad2d5791241f2ff87affb2cb425277d5977
Shweta <Shweta@Shwetas-MacBook-Pro.local>
2015-06-27 13:46:57 -0700

    Random push

--- gett-android/gett/app/src/main/java/summer/hackday/gett/MainActivity.java
+++ gett-android/gett/app/src/main/java/summer/hackday/gett/MainActivity.java
@@ -7,11 +7,15 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
 
 import com.facebook.FacebookSdk;
 
@@ -22,10 +26,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        /** SR:
+         *  Create Parse push notifications
+         * 1. Register to a unique App ID and init Parse
+         * 2. Register for a Push notification service
+          */
         Parse.initialize(this, "RF0OW20w6uLc8K6J1jpg4IZ2AEr5SlQde1ph4WRl", "PvAgMhXiE9cTXt8rxZns5rdJxcMuy0fRAsZRW4SV");
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
+        /** SR: This is where you can add a string to segment the people who you want to send the push notification to.
+         *
+         */
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
 
 
         FacebookSdk.sdkInitialize(this);
