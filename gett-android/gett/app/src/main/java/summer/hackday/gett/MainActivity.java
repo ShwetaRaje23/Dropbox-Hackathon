9f1206e3b30ea4ee07b4a02b9181755345a7c705
muthello <heidibmuth@gmail.com>
2015-06-27 11:47:58 -0700

    Stupid simple main page

--- gett-android/gett/app/src/main/java/summer/hackday/gett/MainActivity.java
+++ gett-android/gett/app/src/main/java/summer/hackday/gett/MainActivity.java
@@ -1,15 +1,15 @@
 package summer.hackday.gett;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.facebook.FacebookSdk;
 
@@ -21,6 +21,36 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(this);
         setContentView(R.layout.activity_main);
+
+        FrameLayout viewPagerContainer = (FrameLayout) findViewById(R.id.cool_view_pager_container);
+        final ViewPager viewPager = new ViewPager(this);
+        viewPager.setAdapter(new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return 3;
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                View item = getLayoutInflater().inflate(R.layout.cool_page_item, null);
+                ImageView itemImage = (ImageView) item.findViewById(R.id.page_item_image);
+                itemImage.setImageResource(R.drawable.placeholder_pickaplace);
+                viewPager.addView(item);
+                return item;
+            }
+
+            @Override
+            public void destroyItem (ViewGroup container, int position, Object object) {
+                container.removeView((View) object);
+            }
+        });
+        viewPagerContainer.addView(viewPager);
+
     }
 
     @Override
