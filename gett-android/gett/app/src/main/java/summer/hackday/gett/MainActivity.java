7f35bec102a37bebf3f5c7cfc64fb96a9484e7a1
muthello <heidibmuth@gmail.com>
2015-06-27 10:29:02 -0700

    Remove appcompat dependency

--- gett-android/gett/app/src/main/java/summer/hackday/gett/MainActivity.java
+++ gett-android/gett/app/src/main/java/summer/hackday/gett/MainActivity.java
@@ -1,12 +1,12 @@
 package summer.hackday.gett;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.view.View;
 
 
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
 
     public void onNotificationTestButton(View view) {
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        Notification.Builder builder = new Notification.Builder(this);
         builder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Gett it!")
                 .setContentText("Heidi is going to TeaTalk!");
